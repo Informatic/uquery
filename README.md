256304a18c7811d94a08ff066da9fd7015a0c64a
Piotr Dobrowolski <admin@tastycode.pl>
2016-10-06 00:07:26 +0200

    xD?

---
+++ README.md
@@ -0,0 +1,15 @@
+uQuery
+======
+Why? Because I fucking can. And I needed that for some embedded use.
+
+Usage
+-----
+Take a look at `index.html`.
+
+License
+-------
+MIT
+
+Disclaimer
+----------
+I'm not a JS developer, ok?
--- uquery.js
+++ uquery.js
@@ -17,8 +17,17 @@ window.uQuery = (function() {
     $.prototype = new Array;
 
     Object.assign($.prototype, {
+        each: function(func) { this.forEach(func); return this; },
+
         css: function(obj) {
+            if(typeof obj == 'string')
+                return this[0].style[obj]
             return this.each(function(c) { Object.assign(c.style, obj); });
+        },
+        attr: function(obj) {
+            if(typeof obj == 'string')
+                return this[0].getAttribute(obj)
+            return this.each(function(c) { for(var n in obj) c.setAttribute(n, obj[n]); });
         },
         on: function(evt, cb) {
             return this.each(function(c) {
@@ -27,15 +36,27 @@ window.uQuery = (function() {
                 });
             });
         },
-        show: function() { return this.css({'visibility': 'initial'}); },
-        hide: function() { return this.css({'visibility': 'hidden'}); },
-        each: function(func) { this.forEach(func); return this; },
+        show: function() { return this.css({'display': 'block'}); }, // FIXME
+        hide: function() { return this.css({'display': 'none'}); },
 
         set: function(attr, value) {
             return this.each(function(c) { c[attr] = value; });
         },
         text: function(d) { return this.set('textContent', d); },
         html: function(d) { return this.set('innerHTML', d); },
+
+        hasClass: function(c) { return this[0].classList.contains(c) },
+        appendClass: function(c) {
+            return this.each(function(e) { e.classList.add(c); });
+        },
+        removeClass: function(c) {
+            return this.each(function(e) { e.classList.remove(c); });
+        },
+        toggleClass: function(c) {
+            return this.each(function(e) {
+                e.classList.contains(c) ? e.classList.remove(c) : c.classList.add(c);
+            });
+        },
     });
 
     Object.assign($, {
@@ -43,10 +64,16 @@ window.uQuery = (function() {
             var req = new XMLHttpRequest();
             req.open(opts.method||'GET', opts.url, true);
             req.onreadystatechange = function(evt) {
-                if(evt.readyState == 4 && req.status == 200)
-                    (opts.success||function(){})(evt);
-                else
-                    (opts.error||function(){})(evt);
+                if(req.readyState == 4) {
+                    if(opts.json)
+                        req.responseJSON = JSON.parse(req.response);
+
+                    (opts.response||function(){})(req);
+                    if(req.status == 200)
+                        (opts.success||function(){})(req);
+                    else
+                        (opts.error||function(){})(req);
+                }
             }
             req.send(opts.data||null);
             return this;
